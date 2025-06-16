d3ea8c0023922b5734d411197130d5775743ed7b
aniketshirsath <aniketshirsath1411@gmail.com>
2025-06-16 17:18:29 +0530

    ok

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -8,28 +8,28 @@ const Projects = () => {
     const fetchedProjects = [
       {
         title: "🛍️ Westside - React E-commerce Clone Website",
-        image: "https://private-user-images.githubusercontent.com/148868961/455466281-4bb1e07c-7db0-4234-bbca-3fc1a55bb0db.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NTAwNzA0NDUsIm5iZiI6MTc1MDA3MDE0NSwicGF0aCI6Ii8xNDg4Njg5NjEvNDU1NDY2MjgxLTRiYjFlMDdjLTdkYjAtNDIzNC1iYmNhLTNmYzFhNTViYjBkYi5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwNjE2JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDYxNlQxMDM1NDVaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1jNTE0ZTFhNWRjOWYwODBjOWE4NjgyYjMxNDdmMDg4OTU4NDVjZDFkOGYxN2M3ZjU3MWZhOTlmYmFkZDk1MTMxJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.eE4t2pgQeyRgcsx5Adg8qQHxK-DcItbxTNWGPPbOX5U",
+        image: "https://www.westside.com/cdn/shop/files/Polo-Web_5f2d056f-0e57-4ed1-b839-325148538f6c.jpg?v=1749792532",
         description: "A stylish fashion e-commerce website with a smooth shopping cart, trendy product pages, and modern UI design.",
         code: "https://github.com/aniketshirsath/Westside_Fe",
         live: "https://westside-fe.vercel.app/",
       },
       {
         title: "🎨 Paul Smith - Frontend Clone Website",
-        image: "https://private-user-images.githubusercontent.com/148868961/454838643-bad9ea47-a1ac-42a7-9786-4e3a07b75468.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDk4MTQ0NjEsIm5iZiI6MTc0OTgxNDE2MSwicGF0aCI6Ii8xNDg4Njg5NjEvNDU0ODM4NjQzLWJhZDllYTQ3LWExYWMtNDJhNy05Nzg2LTRlM2EwN2I3NTQ2OC5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwNjEzJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDYxM1QxMTI5MjFaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1hMmUyZGI0NmU4NWJkNzliZWYxZjEyMzZhNzlkY2U0YmVhYTdiNDkzMmVlZTdjNmQwYzU5MGQyMjFlOTFkYmZmJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.SiheV59i7E6r9gRw9vGXtTvYHEociZ9kTc1GUhDlTZo",
+        image: "https://www.madethought.com/uploads/projects/PAUL-SMITH/_landscape768/Madethought_PAUL-SMITH_CASE-STUDY-1.jpg",
         description: "Welcome to the Paul Smith Clone – a modern, responsive frontend website inspired by the Paul Smith e-commerce design. Built with clean layouts and smooth navigation for a user-friendly shopping experience.",
         code: "https://github.com/aniketshirsath/PaulSmith_Fe",
         live: "https://paul-smith-fe.vercel.app/",
       },
       {
         title: "🎬 CineVibe - Movie Booking Website UI",
-        image: "https://private-user-images.githubusercontent.com/148868961/454011224-9220ec89-cd06-458f-8ccd-fc0435dcf046.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDk5MDIyODUsIm5iZiI6MTc0OTkwMTk4NSwicGF0aCI6Ii8xNDg4Njg5NjEvNDU0MDExMjI0LTkyMjBlYzg5LWNkMDYtNDU4Zi04Y2NkLWZjMDQzNWRjZjA0Ni5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwNjE0JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDYxNFQxMTUzMDVaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1kZjgzOGQwZGZjNjUwMmM2YmYyYWQ4MjYxYzJkMTIxY2UzY2ZjZTMyOTM0OWQ4NTkxOGQxMTEwNmRiYWFmNWU3JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.41M5I-9_JqfmzRew_V1Uogl3qBcIGXHYYfSIXncMFds",
+        image: "https://i.ytimg.com/vi/dyGO7vQw3xM/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBtDlfcEtmWfedrdC3E1iwZukKhtg",
         description: "CineVibe is a modern movie booking website frontend built with HTML, CSS, and Media Queries. It features a clean UI inspired by platforms like BookMyShow and showcases responsive design skills.",
         code: "https://github.com/aniketshirsath/CineVibe",
         live: "https://cine-vibe-omega.vercel.app/",
       },
       {
         title: "🛍️ Casmart - Fashion E-commerce Website",
-        image: "https://private-user-images.githubusercontent.com/148868961/453785981-58a60f31-78cd-470d-b8e1-9d803a505ad0.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDk2MjYwMDUsIm5iZiI6MTc0OTYyNTcwNSwicGF0aCI6Ii8xNDg4Njg5NjEvNDUzNzg1OTgxLTU4YTYwZjMxLTc4Y2QtNDcwZC1iOGUxLTlkODAzYTUwNWFkMC5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwNjExJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDYxMVQwNzA4MjVaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0xOGViNWJmMTE0ZWJiMTVhNDExODAwMzM1Zjg5ZDFlOTdjZTdjYWIyNmI1MTFhYjRjZmMxZDZiNmYzZjlkNTM1JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.c9Hvz_-ySGLiDlhA00iJ4NbVWUoNl0fT9XEOX0WiePs",
+        image: "https://repository-images.githubusercontent.com/489636246/cda881a2-5456-4646-aac2-63718dc43c40",
         description: "Casmart is a stylish and responsive fashion e-commerce frontend made with HTML, CSS, Bootstrap. It’s perfect for online store layouts..",
         code: "https://github.com/aniketshirsath/casmart_web",
         live: "https://casmart-webpage.vercel.app/",
